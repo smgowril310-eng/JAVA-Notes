e2512c240977671eeb2431086ef8bb8c267ee862
Gowri <smgowril310@gmail.com>
2026-07-26 12:40:15 -0400

    Java introduction

---
+++ 01_Introduction/Hello.java
@@ -0,0 +1,87 @@
+//JAVA
+/* Java is a  high-level,Object oriented programming language used  to develop software applications */
+
+//Java Works:(Compie and Run Process)
+/**
+ * Java Source code(.java)
+ *       |
+ * Java Compiler(javac)
+ *       |
+ * Byte code(.class)
+ *       |
+ *     JVM (Run)
+ *       |
+ *    Output
+ */
+
+
+
+//History of JAVA
+/**
+ * JAVA was developed by James Gosling and his team at Sun MicroSystem
+ * It was released in 1995
+ * Later,Sun MicroSystem was acquired by Oracle corporation
+ * Java name => Green Talk -> Oak(National tree) -> Java
+ */
+
+ //Applications of Java
+ /**
+  * Java is used for:
+  * 1.Web Applications:
+      Java is used to build the backend of Websites
+  * 2.Desktop Applications:
+      Java is used to build Software that run on computers
+  * 3.Mobile(or) Android Applications:
+      Java is used to develop Android Mobile Applications
+  * 4.Banking Systems:
+      Java is widely used in banking because it is secure and reliable
+  * 5.E-Commerce Applications:
+      Java is used to build online Shopping Websites
+  * 6.Enterprise Applications:
+      Java is used to build Large business Applications
+  * 7.Cloud Applications:
+      Java is used to develop Applications that on Cloud Servers 
+*/
+
+//Features of Java
+/**
+ * 1.Simple:
+ * Java is easy to learn and understand
+ * 2.Object-Oriented:
+ * Java is based on object and classes
+ * 3.Platform independent:(Write Once Run AnyWhere)
+ * Java code runs on any operating System that has JVM
+ * 4.Secure:
+ * Java provides Strong Security
+ * 5.Robust:
+ * Java handles error eefficiently
+ * 6.MultiThreaded:
+ * Java can execute multiple task simultaneously
+ */
+
+ //JDK Contains
+ /**
+  * Java Development Kit(JDK): Platform dependent
+      JDK is used to develop,compile and run Java Programs
+      JDK = JRE + compiler + Development Tools
+  * Java Virtual Environment(JVM):
+      JVM runs Java Bytecode
+      Just IN Time (JIT):
+
+  * Java Runtime Environment(JRE):
+      JRE provides the environment required to run a Java program
+      JRE = JVM + Libraries
+  */
+
+  //Program Structure
+  public class Hello{ //create a class named Hello
+    public static void main(String[] args) { //This is the entry point of the program
+        System.out.println("Hello,Gowri!"); //Prints the message on the Screen
+    }
+  }
+
+  /**
+   * cd 01_Introduction
+   *  javac Hello.java
+   * java Hello
+   */
